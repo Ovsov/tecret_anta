4254b8067bb8ea7a709429cd142b0b2dc26584c2
Ovsov <aleksei.ovsiannikov@fresha.com>
2024-12-14 11:52:31 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in secret_ovanta.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/secret_ovanta.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "secret_ovanta/version"
+
+module SecretOvanta
+  class Error < StandardError; end
+  # Your code goes here...
+end
